56f3d9f5844d75d212e4351763a90fc16134907c
Nicolas B <brdnicolas@nicolass-macbook-pro.home>
2023-12-23 11:06:43 +0100

    change project to module

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 import { Sequelize } from 'sequelize'
 
-export const sequelize = new Sequelize(
-  'postgres://default:uM0VEZ9mOBno@ep-dry-frog-94221771-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb' +
-    '?sslmode=require'
-)
+export const sequelize = new Sequelize(process.env.POSTGRES_URL + '?sslmode=require')
 
 export const testDbConnection = async () => {
   try {
