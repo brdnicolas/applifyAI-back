769b003a6b7014100605b68415c44ef475c0337a
Nicolas Brouard <brdnicolas.contact@gmail.com>
2023-12-24 02:07:26 +0100

    add swagger

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -2,14 +2,14 @@ import swaggerAutogen from 'swagger-autogen'
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Description'
+    title: 'ApplifyAI API',
+    description: 'API for ApplifyAI'
   },
-  host: 'localhost:3000'
+  host: 'localhost:4000'
 }
 
 const outputFile = './swaggerConfig.json'
-const routes = ['./src/app.ts']
+const routes = ['../app.ts']
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
